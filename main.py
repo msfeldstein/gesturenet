0d08c37e9f96e6e48e53b9a18844b043b2be0928
Mike Feldstein <feldstein@fb.com>
2017-01-17 19:42:29 -0600

    Get a very basic network up and running

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from connect import *
+from train import Trainer
 
+def onPrediction(results):
+    pass
+trainer = new Trainer(onPrediction)
+trainer.train(0, 0)
 def trainCallback(action, data):
     pass
 
--- train.py
+++ train.py
@@ -2,30 +2,32 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 import numpy as np
 
-data_dim = 2
-timesteps = 8
-nb_classes = 10
+class Trainer:
+    def __init__(self, listener):
+        self.listener = listener
+        data_dim = 2
+        timesteps = 8
+        self.nb_classes = 10
 
-# expected input data shape: (batch_size, timesteps, data_dim)
-model = Sequential()
-model.add(LSTM(32, return_sequences=True,
-               input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(32))  # return a single vector of dimension 32
-model.add(Dense(nb_classes, activation='softmax'))
+        # expected input data shape: (batch_size, timesteps, data_dim)
+        self.model = Sequential()
+        self.model.add(LSTM(32, return_sequences=True,
+                       input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
+        self.model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
+        self.model.add(LSTM(32))  # return a single vector of dimension 32
+        self.model.add(Dense(self.nb_classes, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+        self.model.compile(loss='sparse_categorical_crossentropy',
+                      optimizer='rmsprop',
+                      metrics=['accuracy'])
 
-# generate dummy training data
-x_train = np.random.random((1000, timesteps, data_dim))
-y_train = np.random.random((1000, nb_classes))
-
-# generate dummy validation data
-x_val = np.random.random((100, timesteps, data_dim))
-y_val = np.random.random((100, nb_classes))
-
-model.fit(x_train, y_train,
-          batch_size=64, nb_epoch=5,
-          validation_data=(x_val, y_val))
+    def train(self, points, class_value):
+        print("Training " + str(class_value))
+        np_points = np.array([points])
+        y_train = np.array([class_value])
+        self.model.fit(np_points, y_train)
+        prediction = self.model.predict(np_points)
+        print("Should be ", class_value)
+        print("Prediction ", prediction)
+        self.listener(prediction)
+        
